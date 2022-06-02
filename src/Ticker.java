427505294066ee1d15ba6fc89f143522c2dde582
MCNewbie101 <94023224+MCNewbie101@users.noreply.github.com>
2022-06-02 15:06:28 -0700

    2022.6.2 2

---
+++ src/Ticker.java
@@ -0,0 +1,18 @@
+public class Ticker extends Thread {
+    private boolean paused;
+
+    @Override
+    public void run() {
+        while (!paused) {
+            
+        }
+    }
+
+    public boolean isPaused() {
+        return paused;
+    }
+
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
+}
